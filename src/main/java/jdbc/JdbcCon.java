d84f35b6a5e2b4cb17d2ed517c88864584eb542b
joleniuz <49279283+joleniuz@users.noreply.github.com>
2020-05-05 10:34:51 +0200

    IssueItem pushed

---
+++ src/main/java/jdbc/JdbcCon.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Joel
+ */
+public class JdbcCon {
+    
+    private Connection conn;
+    private Statement statement;
+    
+    public static Connection openConnection(){
+        
+        Connection conn = null;
+        
+        if(conn==null){
+            
+            try {
+            // db parameters
+            String url       = "jdbc:mysql://localhost:3306/sakila?useTimezone=true&serverTimezone=UTC";
+            String user      = "root";
+            String password  = "";
+
+            // create a connection to the database
+            conn = DriverManager.getConnection(url, user, password);
+            
+            System.out.println("uppkopplad");
+            // more processing here
+            // ...	
+            } catch(SQLException e) {
+            System.out.println(e.getMessage());
+          
+            }
+            
+        } return conn;
+
+        
+    }
+    
+}
